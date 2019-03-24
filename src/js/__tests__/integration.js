fa3e61a596a9a8f96f887b79a16b60b11d976552
ashwin <ashwinkn@hotmail.com>
2019-03-24 17:35:12 -0500

    generated by     - gen-lib@1.2.9     - @kurlytail/gen-sgen@1.2.9

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -6,26 +6,18 @@ describe('# integration test', () => {
     });
 
     it('## should print help options', () => {
-        const output = execSync('./scripts/sgen-spring.sh -h').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design', () => {
-        const output = execSync('./scripts/sgen-spring.sh -d src/test/fixture/design.json -o testoutput').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design with merge', () => {
-        let output = execSync('./scripts/sgen-spring.sh -d src/test/fixture/design.json -o testoutput').toString();
-        expect(output).toMatchSnapshot();
-        output = execSync('./scripts/sgen-spring.sh -d src/test/fixture/design.json -o testoutput').toString();
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/spring.min.js -h').toString();
         expect(output).toMatchSnapshot();
     });
 
     it('## should generate design and run spring commands', () => {
-        let output = execSync('./scripts/sgen-spring.sh -d src/test/fixture/design.json -o testoutput').toString();
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/spring.min.js -d src/test/fixture/design.json -o testoutput').toString();
+        output = output.replace(/info: Loaded generator .*spring.min.js.*/, '');
         expect(output).toMatchSnapshot();
-        output = execSync('npm install', { cwd: 'testoutput' }).toString();
-        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+        execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run build', { cwd: 'testoutput', stdio: 'inherit' });
     });
 });
