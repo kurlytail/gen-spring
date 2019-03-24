cdb71f9d7cdc5ce0a3782d17ce85d03382811c65
ashwin <ashwinkn@hotmail.com>
2019-03-24 17:58:15 -0500

    Fixed test and snapshots

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Path from 'path';
 // eslint-disable-next-line no-undef
 const requireFunc = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
 const baseModule = requireFunc.main.children.find(module => module.id.includes('gen-spring/dist/spring.min.js'));
+// eslint-disable-next-line no-unused-vars
 let basePath;
 
 if (!baseModule) {
@@ -15,7 +16,7 @@ basePath = Path.relative(process.cwd(), basePath) || '.';
 const options = {
     // eslint-disable-next-line no-undef
     version: app.version,
-    map: [`${basePath}/templates/map.json`]
+    generator: ['pom']
 };
 
 export default options;
