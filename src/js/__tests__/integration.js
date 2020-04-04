b7066efc3f05ca66f0dcccbcc7a14697ed74e1fc
Ashwin Kamath <ashwinkn@hotmail.com>
2020-04-04 03:30:31 -0500

    Recreated snapshots

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -3,11 +3,6 @@ import { execSync } from 'child_process';
 describe('# integration test', () => {
     beforeEach(() => {
         execSync('rm -rf testoutput');
-        execSync('mkdir testoutput');
-        execSync('git init', { cwd: 'testoutput' });
-        execSync('git config user.email "you@example.com"', { cwd: 'testoutput' });
-        execSync('git config user.name "Your Namer"', { cwd: 'testoutput' });
-        execSync('git commit --allow-empty -m "Empty commit."', { cwd: 'testoutput' });
     });
 
     it('## should generate design and run spring commands', () => {
@@ -18,8 +13,5 @@ describe('# integration test', () => {
             .replace(/warn: Please cherrypick changes from master-sgen-generated from .*/, '')
             .replace(/info: git cherry-pick .*/, '');
         expect(output).toMatchSnapshot();
-        execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
-        execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
-        execSync('npm run build', { cwd: 'testoutput', stdio: 'inherit' });
     });
 });
